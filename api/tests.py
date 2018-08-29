268caf7c8a15c05dd9b1b63c5c91f0403d94757a
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-08-29 09:49:52 -0500

    Clean up tests for 'Review' model

--- api/tests.py
+++ api/tests.py
@@ -7,8 +7,8 @@ from api.serializers import ReviewSerializer
 
 
 class ReviewModelTests(TestCase):
-    def test_can_create_review(self):
-        review = Review(
+    def setUp(self):
+        self.review = Review(
             title='My review',
             summary='This is my first review.',
             rating=1,
@@ -16,89 +16,48 @@ class ReviewModelTests(TestCase):
             company='Some Company',
             reviewer='Some Reviewer',
         )
-        review.save()
 
-        self.assertIsNotNone(review.id)
-        self.assertEqual(review.title, 'My review')
-        self.assertEqual(review.summary, 'This is my first review.')
-        self.assertEqual(review.rating, 1)
-        self.assertEqual(review.ip_address, '127.0.0.1')
-        self.assertEqual(review.company, 'Some Company')
-        self.assertEqual(review.reviewer, 'Some Reviewer')
+    def test_save_model(self):
+        self.review.save()
+
+        self.assertIsNotNone(self.review.id)
+        self.assertEqual(self.review.title, 'My review')
+        self.assertEqual(self.review.summary, 'This is my first review.')
+        self.assertEqual(self.review.rating, 1)
+        self.assertEqual(self.review.ip_address, '127.0.0.1')
+        self.assertEqual(self.review.company, 'Some Company')
+        self.assertEqual(self.review.reviewer, 'Some Reviewer')
 
         iso_re = re.compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,6}[+-]{1}\d{2}:\d{2}')
-        self.assertIsNotNone(iso_re.match(str(review.created_at)))
 
-        reviews = Review.objects.count()
+        self.assertIsNotNone(iso_re.match(str(self.review.created_at)))
+        self.assertEqual(Review.objects.count(), 1)
 
-        self.assertEqual(reviews, 1)
+    def test_save_valid_model(self):
+        self.review.title = 'This is a very very very very very very very veeeeery long title'
+        self.summary = ''.join(['a' for i in range(10000)])
+        self.review.save()
+        self.assertEqual(Review.objects.count(), 1)
 
-    def test_can_create_valid_review(self):
-        review = Review(
-            title='This is a very very very very very very very veeeeery long title',
-            summary=''.join(['a' for i in range(10000)]),
-            rating=1,
-            ip_address='127.0.0.1',
-            company='Some Company',
-            reviewer='Some Reviewer',
-        )
-        review.save()
-
-        reviews = Review.objects.count()
-
-        self.assertEqual(reviews, 1)
-
-    def test_cannot_create_review_long_title(self):
-        review = Review(
-            title='This is a very very very very very very very very very long title',
-            summary='This is my first review.',
-            rating=1,
-            ip_address='127.0.0.1',
-            company='Some Company',
-            reviewer='Some Reviewer',
-        )
-
-        with self.assertRaises(Exception) as cm:
-            review.save()
-
-    def test_cannot_create_review_long_summary(self):
-        review = Review(
-            title='My review',
-            summary=''.join(['a' for i in range(10001)]),
-            rating=1,
-            ip_address='127.0.0.1',
-            company='Some Company',
-            reviewer='Some Reviewer',
-        )
-
-        with self.assertRaises(Exception) as cm:
-            review.save()
-
-    def test_cannot_create_review_below_zero_rating(self):
-        review = Review(
-            title='My review',
-            summary='This is my first review.',
-            rating=-1,
-            ip_address='127.0.0.1',
-            company='Some Company',
-            reviewer='Some Reviewer',
-        )
-
+    def test_validate_long_title(self):
+        self.review.title = 'This is a very very very very very very very very very long title'
         with self.assertRaises(Exception):
-            review.save()
+            self.review.save()
 
-    def test_cannot_create_review_invalid_ip_address(self):
-        review = Review(
-            title='My review',
-            summary='This is my first review.',
-            rating=1,
-            ip_address='127,0,0,1',
-            company='Some Company',
-            reviewer='Some Reviewer',
-        )
-
+    def test_validate_long_summary(self):
+        self.review.summary = ''.join(['a' for i in range(10001)])
         with self.assertRaises(Exception):
-            review.save()
+            self.review.save()
+
+    def test_validate_below_zero_rating(self):
+        self.review.rating = -1
+        with self.assertRaises(Exception):
+            self.review.save()
+
+    def test_validate_invalid_ip_address(self):
+        self.review.ip_address='127,0,0,1'
+        with self.assertRaises(Exception):
+            self.review.save()
 
 
 class ReviewSerializerTests(TestCase):
