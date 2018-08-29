afc11619a67a02c7d6d8d55ae45cb89c68528cc2
Jose Miguel Venegas Mendoza <jvenegasmendoza@gmail.com>
2018-08-29 09:33:20 -0500

    Add serializer for 'Review' model

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,9 @@
 import re
 from django.test import TestCase
+from rest_framework import serializers
 
 from api.models import Review
+from api.serializers import ReviewSerializer
 
 
 class ReviewModelTests(TestCase):
@@ -97,3 +99,88 @@ class ReviewModelTests(TestCase):
 
         with self.assertRaises(Exception):
             review.save()
+
+
+class ReviewSerializerTests(TestCase):
+    def setUp(self):
+        self.review = Review(
+            title='My review',
+            summary='This is my first review.',
+            rating=1,
+            ip_address='127.0.0.1',
+            company='Some Company',
+            reviewer='Some Reviewer',
+        )
+
+        self.data = {
+            'id': 1,
+            'title': 'My review',
+            'summary': 'This is my first review.',
+            'rating': 1,
+            'ip_address': '127.0.0.1',
+            'company': 'Some Company',
+            'reviewer': 'Some Reviewer',
+        }
+
+    def test_serialize_model(self):
+        self.review.save()
+        serializer = ReviewSerializer(self.review)
+
+        self.assertIsInstance(serializer, serializers.ModelSerializer)
+
+        data = serializer.data
+
+        self.assertIsNotNone(data.get('id'))
+        self.assertEqual(data.get('title'), 'My review')
+        self.assertEqual(data.get('summary'), 'This is my first review.')
+        self.assertEqual(data.get('rating'), 1)
+        self.assertEqual(data.get('ip_address'), '127.0.0.1')
+        self.assertEqual(data.get('company'), 'Some Company')
+        self.assertEqual(data.get('reviewer'), 'Some Reviewer')
+
+    def test_deserialize_data(self):
+        serializer = ReviewSerializer(data=self.data)
+
+        self.assertIsInstance(serializer, serializers.ModelSerializer)
+        self.assertTrue(serializer.is_valid())
+
+        validated_data = serializer.validated_data
+
+        self.assertEqual(validated_data.get('title'), 'My review')
+        self.assertEqual(validated_data.get('summary'), 'This is my first review.')
+        self.assertEqual(validated_data.get('rating'), 1)
+        self.assertEqual(validated_data.get('ip_address'), '127.0.0.1')
+        self.assertEqual(validated_data.get('company'), 'Some Company')
+        self.assertEqual(validated_data.get('reviewer'), 'Some Reviewer')
+
+    def test_validate_long_title(self):
+        self.data['title'] = 'This is a very very very very very very very very very long title'
+        serializer = ReviewSerializer(data=self.data)
+
+        self.assertIsInstance(serializer, serializers.ModelSerializer)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors.get('title')), 1)
+
+    def test_validate_long_summary(self):
+        self.data['summary'] = ''.join(['a' for i in range(10001)])
+        serializer = ReviewSerializer(data=self.data)
+
+        self.assertIsInstance(serializer, serializers.ModelSerializer)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors.get('summary')), 1)
+
+    def test_validate_below_zero_rating(self):
+        self.data['rating'] = -1
+        serializer = ReviewSerializer(data=self.data)
+
+        self.assertIsInstance(serializer, serializers.ModelSerializer)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors.get('rating')), 1)
+
+    def test_validate_invalid_ip_address(self):
+        self.data['ip_address'] = '127,0,0,1'
+        serializer = ReviewSerializer(data=self.data)
+
+        self.assertIsInstance(serializer, serializers.ModelSerializer)
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(len(serializer.errors.get('ip_address')), 1)
